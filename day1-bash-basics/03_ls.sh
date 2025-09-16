57439282470e0233c48e0ba358fc2261166362ff
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-16 15:56:42 +0600

    feat(bash): ls and cd command navigation

---
+++ day1-bash-basics/03_ls.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ls command lists files and directories
+# Author: <Nahid Hasan>
+# Date: 16.09.2025
+ls
+ls -l        # long format
+ls -a        # show hidden files
+ls -lh       # human-readable sizes
---
+++ day1-bash-basics/04_cd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# cd command changes directories
+# Author: <Nahid Hasan>
+# Date: 16.09.2025
+echo "Current directory: $(pwd)"
+cd /tmp
+echo "Now in directory: $(pwd)"
