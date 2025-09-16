51a56664ab45b58b5b5769c665f94bf3f2a2371a
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-16 15:55:51 +0600

    feat(bash): echo + pwd command usages

---
+++ day1-bash-basics/01_echo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# echo command prints text to the terminal
+# Author: <Nahid Hasan>
+# Date: 16.09.2025
+
+echo "Hello, World!"
+echo "This is Day 1 of Bash learning."
---
+++ day1-bash-basics/02_pwd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# pwd command prints the current working directory
+# Author: <Nahid Hasan>
+# Date: 16.09.2025
+pwd
