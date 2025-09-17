1c47ad9350b94bd913b1e25cbbd2e5d93251df4f
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-17 22:03:38 +0600

    feat(bash): simple calculation and results Show

--- day2-variables-input/user_info_calc.sh
+++ day2-variables-input/user_info_calc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Day 2 Project: Variables, Strings, Numbers, User Input
+# Author: <Nahid Hasan>
+# Date: 17.09.2025
 
 echo "=== Welcome to User Info & Calculator Script ==="
 
@@ -9,3 +11,18 @@ read -p "Enter your age: " age
 
 # Use variables in a string
 echo "Hello, $name! You are $age years old."
+# Simple calculation
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+# Arithmetic
+sum=$((num1 + num2))
+product=$((num1 * num2))
+
+# Show results
+echo "✅ Results:"
+echo "$num1 + $num2 = $sum"
+echo "$num1 * $num2 = $product"
+
+# String length example
+echo "Your name has ${#name} characters."
