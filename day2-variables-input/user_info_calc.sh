015df8462cac99aa304943f2c22bf8d49e8a2e4d
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-17 22:01:39 +0600

    added variable + string + user info

---
+++ day2-variables-input/user_info_calc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Day 2 Project: Variables, Strings, Numbers, User Input
+
+echo "=== Welcome to User Info & Calculator Script ==="
+
+# Read user info
+read -p "Enter your name: " name
+read -p "Enter your age: " age
+
+# Use variables in a string
+echo "Hello, $name! You are $age years old."
