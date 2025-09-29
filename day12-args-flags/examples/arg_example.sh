4c4c73cfea2797c8b789d509e75aed4f9f58c9a6
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-29 19:25:23 +0600

    feat(bash): Simple arguments

---
+++ day12-args-flags/examples/arg_example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Example: Simple arguments
+# Author: <Nahid Hasan>
+# Date: 29.09.2025
+
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "All arguments: $@"
+echo "Number of arguments: $#"
